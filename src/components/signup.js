20687890068f4c2e12ef3788fadf83824cccf2dc
Gustavo Caetano <CaetanooG@hotmail.com>
2022-01-04 21:00:21 +0100

    feat(core): Create registration request
    Use axios to post a new user into the DB via post request

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,25 +1,59 @@
 import React from "react";
 import '../assets/css/signup.css'
-import {Label, Input, Form, FormGroup, Button} from 'reactstrap';
+import {Label, Input, Form, FormGroup, Button, Alert} from 'reactstrap';
 import Wave from "react-wavify";
 import Logo from "./logo";
 import PasswordStrengthMeter from "./passwordStrengthMeter";
 import {useState} from "react";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 export const Signup = () => {
 
+    let navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (password != passwordConfirmation)
+            setConfirmationState(true);
+        else
+            setConfirmationState(false);
+
+        const data = {
+            email: email,
+            name: name,
+            password: password,
+            passwordConfirmation: passwordConfirmation
+        }
+
+        axios.post('http://localhost:3005/auth/signup', data)
+            .then(response => {
+                if(response.status === 201)
+                    navigate('/');
+            }
+            ).catch(e => {
+                console.log(e);
+        });
+    }
+
     const [password, setPassword] = useState('');
-    console.log(password);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [passwordConfirmation, setPasswordConfirmation] = useState('');
+    const [confirmationState, setConfirmationState] = useState(false);
     return (
         <>
             <div className="container-signup">
                 <Logo/>
-                <Form className="signup-form">
+                <Form className="signup-form" onSubmit={handleSubmit}>
                     <h3 className="text-center p-2 registration-message">Registration</h3>
                     <div className="row">
                         <div className="col-sm">
                             <FormGroup>
-                                <Input placeholder="Full name" required/>
+                                <Input placeholder="Full name"
+                                       required
+                                       onChange={e => setName(e.target.value)}/>
                             </FormGroup>
                             <FormGroup>
                                 <Input type="password"
@@ -31,10 +65,16 @@ export const Signup = () => {
                         </div>
                         <div className="col-sm">
                             <FormGroup>
-                                <Input type="email" placeholder="E-mail" required/>
+                                <Input type="email"
+                                       placeholder="E-mail"
+                                       required
+                                       onChange={e => setEmail(e.target.value)}/>
                             </FormGroup>
                             <FormGroup>
-                                <Input type="password" placeholder="Password confirmation" required/>
+                                <Input type="password"
+                                       placeholder="Password confirmation"
+                                       required
+                                       onChange={e => setPasswordConfirmation(e.target.value)}/>
                             </FormGroup>
                         </div>
                     </div>
@@ -47,7 +87,12 @@ export const Signup = () => {
                         </FormGroup>
                     </div>
                     <div className="row">
-                        <Button className="signup-btn btn-dark w-50">Sign up</Button>
+                        <Alert color="danger" isOpen={confirmationState}>
+                            The passwords MUST be equal !
+                        </Alert>
+                    </div>
+                    <div className="row">
+                        <Button className="signup-btn btn-dark w-50" type="submit">Sign up</Button>
                         <div className="text-center pt-3"> Do you already have an account?</div>
                         <div className="text-center">
                             <a href="/">Sign In</a>
